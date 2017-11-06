c181c84decf49664efb2934140b6f23405a9e807
morumama <33329266+morumama@users.noreply.github.com>
2017-11-07 00:18:27 +0900

    20171107

--- js/rpg_windows.js
+++ js/rpg_windows.js
@@ -1,4 +1,4 @@
-﻿//=============================================================================
+//=============================================================================
 // rpg_windows.js v1.5.1
 //=============================================================================
 
@@ -585,12 +585,12 @@ Window_Base.prototype.drawActorSimpleStatus = function(actor, x, y, width) {
     var lineHeight = this.lineHeight();
     var x2 = x + 180;
     var width2 = Math.min(200, width - 180 - this.textPadding());
- //this.drawActorName(actor, x, y);
- //this.drawActorLevel(actor, x, y + lineHeight * 1);
- //this.drawActorIcons(actor, x, y + lineHeight * 2);
- //this.drawActorClass(actor, x2, y);
- //this.drawActorHp(actor, x2, y + lineHeight * 1, width2);
- //this.drawActorMp(actor, x2, y + lineHeight * 2, width2);
+    this.drawActorName(actor, x, y);
+    this.drawActorLevel(actor, x, y + lineHeight * 1);
+    this.drawActorIcons(actor, x, y + lineHeight * 2);
+    this.drawActorClass(actor, x2, y);
+    this.drawActorHp(actor, x2, y + lineHeight * 1, width2);
+    this.drawActorMp(actor, x2, y + lineHeight * 2, width2);
 };
 
 Window_Base.prototype.drawItemName = function(item, x, y, width) {
@@ -1536,7 +1536,7 @@ Window_Gold.prototype.refresh = function() {
     var x = this.textPadding();
     var width = this.contents.width - this.textPadding() * 2;
     this.contents.clear();
-    this.drawCurrencyValue(this.value(), this.currencyUnit(), x, 0, width);this.visible = false;
+    this.drawCurrencyValue(this.value(), this.currencyUnit(), x, 0, width);
 };
 
 Window_Gold.prototype.value = function() {
